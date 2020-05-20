b35a3edea86a267a5b95b2d353e78cee22d020ab
charisTheo <dev@charistheo.io>
2020-05-20 15:55:23 +0100

    Updates

---
+++ precache-manifest.505efe48ff629d71ce8556ac3d9a03c6.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c5b1acf6f4835c25685fc0c0bf709c",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "140629a3f68b3e1dd4fcacbf23463ea1",
+    "url": "favicon/android-chrome-192x192.png"
+  },
+  {
+    "revision": "7e53eed74e39034019dd17d9e6664635",
+    "url": "favicon/android-chrome-512x512.png"
+  },
+  {
+    "revision": "7c116792e0476df1033ea436ed911dc8",
+    "url": "favicon/apple-touch-icon.png"
+  },
+  {
+    "revision": "b17849ae615fc2b32bac3e52f108415f",
+    "url": "favicon/favicon-16x16.png"
+  },
+  {
+    "revision": "1cb34ef7c5cb10f3c7526889d1e4ee30",
+    "url": "favicon/favicon-32x32.png"
+  },
+  {
+    "revision": "0eba0dfb31b86c9d6f7bf44d1bc4513e",
+    "url": "favicon/favicon-64x64.png"
+  },
+  {
+    "revision": "09c5b1acf6f4835c25685fc0c0bf709c",
+    "url": "favicon/favicon.ico"
+  },
+  {
+    "revision": "eed45d9fdb1e6bc391b7d12006b982b4",
+    "url": "favicon/logo.svg"
+  },
+  {
+    "revision": "0e3dad01d1a48d2dc42bf6fee172d421",
+    "url": "favicon/maskable_icon.png"
+  },
+  {
+    "revision": "60b0d2ef4d54c681c361b2376ddcff91",
+    "url": "favicon/twa_maskable_icon.png"
+  },
+  {
+    "revision": "1d9a793c6dc1692e45ec527f92fdb646",
+    "url": "img/placeholder-image.png"
+  },
+  {
+    "revision": "6d6119fda62054c1dac7fa957a0014a8",
+    "url": "img/pwa-logo.png"
+  },
+  {
+    "revision": "c450453e2e1948cfb64b9ac801cd786b",
+    "url": "img/share-24px.svg"
+  },
+  {
+    "revision": "7728902b9ec51ce8583bd07e4080e559",
+    "url": "index.html"
+  },
+  {
+    "revision": "d04137b5709ff6d9cd5713a9eb41110b",
+    "url": "js/highlight.pack.js"
+  },
+  {
+    "revision": "7c7ee627cc6217b2142375b2ee09d9e7",
+    "url": "js/index.notused.js"
+  },
+  {
+    "revision": "259541f49eed9e3f81fe0a260f0de553",
+    "url": "js/mwc-pwa-reload.js"
+  },
+  {
+    "revision": "2477ee4fe8c8ab4f1d2b5fdbe5261484",
+    "url": "js/util.js"
+  },
+  {
+    "revision": "9d6b63e60bd357ccf65ed072a6f105d7",
+    "url": "js/util.notused.js"
+  },
+  {
+    "revision": "8f879f4ccffff2661609ea1222173814",
+    "url": "js/webShare.notused.js"
+  },
+  {
+    "revision": "7a6833831df601de8c44",
+    "url": "main.js"
+  },
+  {
+    "revision": "f36662db29ddeeed76464d6d5a468427",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "2e189d1cc56d10d82fb23a6b0c60dd87",
+    "url": "offline.html"
+  },
+  {
+    "revision": "1d9a793c6dc1692e45ec527f92fdb646",
+    "url": "placeholder-image.png"
+  },
+  {
+    "revision": "a32fb5c3aefeb86bef01c207c0457b82",
+    "url": "pwa-wiki_files/css.min.js"
+  },
+  {
+    "revision": "0e6ff0b15b94d2b2786aca33a494691b",
+    "url": "pwa-wiki_files/gradients_5.png"
+  },
+  {
+    "revision": "c3a41252a43342577903313720e2cce1",
+    "url": "pwa-wiki_files/highlight.min.js"
+  },
+  {
+    "revision": "ce70550254cecb577c270825a4b01853",
+    "url": "pwa-wiki_files/htmlbars.min.js"
+  },
+  {
+    "revision": "bfe0eccb36fba63e11820368352c2c1f",
+    "url": "pwa-wiki_files/javascript.min.js"
+  },
+  {
+    "revision": "eed45d9fdb1e6bc391b7d12006b982b4",
+    "url": "pwa-wiki_files/logo.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.91bad3b0688056919fe773fd6b64212a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.505efe48ff629d71ce8556ac3d9a03c6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // https://developers.google.com/web/tools/workbox/guides/configure-workbox
 
